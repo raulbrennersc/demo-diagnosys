75332720f8ce68c09d7d3f900c59d0fc027664ef
Raul Costa <raulbrennersc@gmail.com>
2020-05-02 20:54:16 -0300

    Instalando pacotes e criando service de autenticação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'autenticacao', loadChildren: () => import('./autenticacao/autenticacao.module').then(m => m.AutenticacaoModule) }];
+const routes: Routes = [
+  { path: 'autenticacao', loadChildren: () => import('./autenticacao/autenticacao.module').then(m => m.AutenticacaoModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
