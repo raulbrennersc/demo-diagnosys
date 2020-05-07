e307c8887b3e0d8ffa68d4b1897736a182d0b610
Raul Costa <raulbrennersc@gmail.com>
2020-05-07 01:23:09 -0300

    Navegando entre etapas

--- src/app/dashboard/fazendas/cadastro/cadastro.component.ts
+++ src/app/dashboard/fazendas/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FazendasService } from 'src/app/_services/fazenda.service';
+import { StaticService } from 'src/app/_services/static.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,38 +9,52 @@ import { FazendasService } from 'src/app/_services/fazenda.service';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
-  fazenda: any = {};
-  
-  constructor(private activatedRoute: ActivatedRoute, private router: Router, private fazendaService: FazendasService) { }
-  
+  fazenda: any = { idEtapa: 1 };
+  etapas: any = [];
+  etapaAtiva = 1;
+
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private fazendaService: FazendasService, private staticService: StaticService) { }
+
   ngOnInit(): void {
     this.carregarFazenda();
   }
 
-  avancarEtapa () {
+  avancarEtapa() {
     this.carregarFazenda();
   }
 
-  carregarFazenda(idNovaFazenda = undefined){
-    if(idNovaFazenda){
+  carregarFazenda(idNovaFazenda = undefined) {
+    if (idNovaFazenda) {
       this.router.navigate(['painel', 'fazendas', 'cadastro', idNovaFazenda]);
     }
 
     this.activatedRoute.params.subscribe(params => {
       const idFazenda = params['id'];
-      if(idFazenda){
+      if (idFazenda) {
         this.fazendaService.consultarFazenda(idFazenda)
+          .subscribe(response => {
+            this.fazenda = response;
+            this.etapaAtiva = this.fazenda.idEtapa;
+          }, response => {
+            alert(response.error);
+            this.router.navigate(['']);
+          });
+      }
+
+      this.staticService.listarEtapasFazenda()
         .subscribe(response => {
-          this.fazenda = response;
+          this.etapas = response;
         }, response => {
           alert(response.error);
-          this.router.navigate(['']);
-        });
-      }
+        })
     });
   }
 
-  concluirCadastro(){
+  concluirCadastro() {
     this.router.navigate(['painel', 'fazendas']);
+  }
+
+  alterarEtapa(idEtapa) {
+    this.etapaAtiva = idEtapa;
   }
 }
