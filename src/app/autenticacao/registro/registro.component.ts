f6c759b303dc608bc008986dec0fccf1dcfd81fb
Raul Costa <raulbrennersc@gmail.com>
2020-05-25 21:49:16 -0300

    Validação do formulário de registro

--- src/app/autenticacao/registro/registro.component.ts
+++ src/app/autenticacao/registro/registro.component.ts
@@ -12,12 +12,20 @@ import { NgForm } from '@angular/forms';
 export class RegistroComponent implements OnInit {
 
   model: any = {};
+  formSubmited = false;
   constructor(private autenticacaoService: AutenticacaoService, private router: Router, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
-  register() {
+  register(form: NgForm) {
+
+    if(form.invalid){
+      this.formSubmited = true;
+      this.alertify.error('Preencha os campos corretamente.');
+      return;
+    }
+
     this.autenticacaoService.register(this.model).subscribe(next => {
       this.alertify.success('Registro realizado!');
       this.router.navigate(['']);
@@ -26,7 +34,7 @@ export class RegistroComponent implements OnInit {
   }
 
   togglePassword(event){
-    const type = event.target.parentElement.parentElement.children['senha'].type
+    const type = event.target.parentElement.parentElement.children['senha'].type;
     if(type == 'password'){
       event.target.parentElement.parentElement.children['senha'].type = 'text';
     }
